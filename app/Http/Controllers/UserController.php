b95a1e6056eaec8b3d3374b9e29f478f30bbadb4
kazulu <antonioacunadev@gmail.com>
2020-05-17 21:46:06 +0200

    Modificado UserController, refactor en show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,15 @@ class UserController extends Controller
 {
     public function show(User $user = null) {
         if (! $user) {
-            return auth()->user();
+            $user = auth()->user();
+        } else if (auth()->user()->followings->contains($user)) {
+            $user->followed = auth()->user()->followings->contains($user);
         }
 
-        $user->followed = auth()->user()->followings->contains($user);
+        // TODO: Now we send the followers and followings in response, fix this to send just the number
+        $user->followerCount = $this->getTotalFollowers($user);
+        $user->followingCount = $this->getTotalFollowings($user);
+
         return $user;
     }
 
@@ -19,8 +24,16 @@ class UserController extends Controller
         return $user->followers;
     }
 
+    public function getTotalFollowers($user) {
+        return count($user->followers);
+    }
+
     public function followings(User $user) {
         return $user->followings;
+    }
+
+    public function getTotalFollowings($user) {
+        return count($user->followings);
     }
 
     public function toogleFollow(User $user) {
