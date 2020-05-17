d32dc10f3ac0bb3fcef8d46d99776971dac8c575
kazulu <antonioacunadev@gmail.com>
2020-05-17 15:59:01 +0200

    Añadidos paquetes Laravel UI y Laravel Sanctum

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => [
+        'sanctum/csrf-token',
+        'api/*'
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +32,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
