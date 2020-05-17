4589b0712fc18687a797460e137dad634242c081
kazulu <antonioacunadev@gmail.com>
2020-05-17 16:08:17 +0200

    Creado el modelo user con migration, factory y seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->text('description')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nickname')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
