da14a52fbedbc4a77002f9fde8f1c9227f7dd0de
kazulu <antonioacunadev@gmail.com>
2020-05-21 12:10:28 +0200

    Modificado routes/api y RegisterController, añadida route y modificado validador para registrar usuarios

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', 'Auth\LoginController@login');
+Route::post('/register', 'Auth\RegisterController@register');
 Route::post('/logout', 'Auth\LoginController@logout');
 
 Route::middleware('auth:sanctum')->group(function() {
